ef7bcfda883b6db634ed81aea04c918fbc0a88d6
teonateona12 <teona.piranishvili1@gmail.com>
2023-05-03 22:00:22 +0400

    feat: work with add data button

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,87 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { useDataStore, User } from "./store";
-import { Table, Button, Space } from "antd";
+import { Table, Button, Space, Modal, Form, Input, Select } from "antd";
+const { Option } = Select;
 const { Column } = Table;
 
 function App() {
   const { data, loadData, addData, updateData, deleteData } = useDataStore();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    gender: "",
+    address: {
+      street: "",
+      city: "",
+    },
+    phone: "",
+  });
+
+  const handleInputChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  const handleAddressChange = (field: any, value: any) => {
+    setFormData({
+      ...formData,
+      address: {
+        ...formData.address,
+        [field]: value,
+      },
+    });
+  };
+  const handleGenderChange = (value: any) => {
+    setFormData({
+      ...formData,
+      gender: value,
+    });
+  };
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleAddData = async () => {
+    const newData = {
+      ...formData,
+      id: data.length + 1,
+    };
+    await addData(newData);
+    setFormData({
+      name: "",
+      email: "",
+      gender: "",
+      address: {
+        street: "",
+        city: "",
+      },
+      phone: "",
+    });
+    setIsModalVisible(false);
+  };
 
   useEffect(() => {
     loadData();
   }, []);
 
-  const handleAddData = async () => {
-    const newData = {
-      id: 101,
-      name: "John Doe",
-      email: "johndoe@example.com",
-      gender: "male",
-      address: { street: "123 Main St", city: "Anytown" },
-      phone: "+1 (555) 555-5555",
-    };
-    await addData(newData);
-  };
+  // const handleAddData = async () => {
+  //   const newData = {
+  //     id: 101,
+  //     name: "John Doe",
+  //     email: "johndoe@example.com",
+  //     gender: "male",
+  //     address: { street: "123 Main St", city: "Anytown" },
+  //     phone: "+1 (555) 555-5555",
+  //   };
+  //   await addData(newData);
+  // };
   const handleUpdateData = async (id: number, data: any) => {
     const updatedData = {
       id: 1,
@@ -39,6 +99,9 @@ function App() {
 
   return (
     <>
+      <Button type="primary" onClick={() => setIsModalVisible(true)}>
+        Add Data
+      </Button>
       <Table dataSource={data} rowKey="id">
         <Column title="Id" dataIndex="id" key="id" />
         <Column title="Name" dataIndex="name" key="name" />
@@ -63,6 +126,96 @@ function App() {
           )}
         />
       </Table>
+      <Modal
+        title="Add Data"
+        open={isModalVisible}
+        onOk={handleAddData}
+        onCancel={() => setIsModalVisible(false)}
+      >
+        <Form layout="vertical">
+          <Form.Item label="Name">
+            <Input
+              name="name"
+              value={formData.name}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  name: e.target.value,
+                })
+              }
+            />
+          </Form.Item>
+          <Form.Item label="Email">
+            <Input
+              name="email"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  email: e.target.value,
+                })
+              }
+            />
+          </Form.Item>
+          <Form.Item label="Gender">
+            <Select
+              value={formData.gender}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  gender: e,
+                })
+              }
+            >
+              <Option value="male">Male</Option>
+              <Option value="female">Female</Option>
+              <Option value="other">Other</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Street">
+            <Input
+              name="street"
+              value={formData.address.street}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  address: {
+                    ...formData.address,
+                    street: e.target.value,
+                  },
+                })
+              }
+            />
+          </Form.Item>
+          <Form.Item label="City">
+            <Input
+              name="city"
+              value={formData.address.city}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  address: {
+                    ...formData.address,
+                    city: e.target.value,
+                  },
+                })
+              }
+            />
+          </Form.Item>
+          <Form.Item label="Phone">
+            <Input
+              name="email"
+              value={formData.phone}
+              onChange={(e) =>
+                setFormData({
+                  ...formData,
+                  phone: e.target.value,
+                })
+              }
+            />
+          </Form.Item>
+        </Form>
+      </Modal>
     </>
   );
 }
