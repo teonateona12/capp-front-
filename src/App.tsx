1c4c0fb6f1518b2c6672da67a7397049fb44a950
teonateona12 <teona.piranishvili1@gmail.com>
2023-05-03 17:00:09 +0400

    feat: work with post request

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,25 @@ import axios from "axios";
 
 function App() {
   const [data, setData] = useState();
-  console.log(data);
   useEffect(() => {
     const getItemsList = async () => {
       const res = await axios.get("http://localhost:3001/api/getdata");
       setData(res.data);
     };
     getItemsList();
-  }, [data]);
+  }, []);
+  const addItem = async (e: any) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/api/addata",
+        "data object"
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return <></>;
 }
 
